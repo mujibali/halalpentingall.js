e14a52b3b6c595c333f823367e6938426551e6d3
mujibali <jib4ly@gmail.com>
2017-01-25 23:12:55 +0700

    Create scriptcokies.js

---
+++ scriptcokies.js
@@ -0,0 +1,20 @@
+<link href='//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css' rel='stylesheet' type='text/css'/>
+<script src='//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js'/>
+<script>
+window.addEventListener(&quot;load&quot;, function(){
+window.cookieconsent.initialise({
+  &quot;palette&quot;: {
+    &quot;popup&quot;: {
+      &quot;background&quot;: &quot;#000&quot;
+    },
+    &quot;button&quot;: {
+      &quot;background&quot;: &quot;#f1d600&quot;
+    }
+  },
+  &quot;position&quot;: &quot;top&quot;,
+  &quot;static&quot;: true,
+  &quot;content&quot;: {
+    &quot;href&quot;: &quot;http://halalpenting.blogspot.co.id/p/blog-page.html&quot;
+  }
+})});
+</script>
